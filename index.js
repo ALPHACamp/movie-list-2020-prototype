49121bb9e6e75545ac1360bd19e989c0a24499b3
Ellen Lee <leechinghui.tw@gmail.com>
2018-08-17 01:11:49 +0800

    display movie list and card

--- index.js
+++ index.js
@@ -4,8 +4,27 @@
   const POSTER_URL = BASE_URL + '/posters/'
   const data = []
 
+  const dataPanel = document.getElementById('data-panel')
+
   axios.get(INDEX_URL).then((response) => {
     data.push(...response.data.results)
-    console.log(data)
+    displayDataList(data)
   }).catch((err) => console.log(err))
+
+  function displayDataList (data) {
+    let htmlContent = ''
+    data.forEach(function (item, index) {
+      htmlContent += `
+        <div class="col-sm-3">
+          <div class="card mb-2">
+            <img class="card-img-top " src="${POSTER_URL}${item.image}" alt="Card image cap">
+            <div class="card-body movie-item-body">
+              <h6 class="card-title">${item.title}</h5>
+            </div>
+          </div>
+        </div>
+      `
+    })
+    dataPanel.innerHTML = htmlContent
+  }
 })()
