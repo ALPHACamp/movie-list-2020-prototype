1df435d0cf85f042906e7e5cdbbd5cd19035da78
Ellen Lee <leechinghui.tw@gmail.com>
2018-08-17 00:20:29 +0800

    access movie index api

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 (function () {
-  // write your code here
+  const BASE_URL = 'https://movie-list.alphacamp.io'
+  const INDEX_URL = BASE_URL + '/api/v1/movies/'
+  const POSTER_URL = BASE_URL + '/posters/'
+  const data = []
+
+  axios.get(INDEX_URL).then((response) => {
+    data.push(...response.data.results)
+    console.log(data)
+  }).catch((err) => console.log(err))
 })()
